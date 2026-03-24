84c5ec7ab7817d3cf3f0e5eb6f77e127557719eb
Nick Papior <nickpapior@gmail.com>
2026-03-24 13:53:19 +0100

    initial attempt to build and create an installer
    The recipe has been changed to accommodate the
    problems related to having separate repos.
    
    In order to retain a minimal effort to sustain
    the development we are using the miniforge-constructor
    repo as a sub-module. In this way we do not have
    to handle merge-conflicts.
    It required a simple mechanism to bypass the yaml
    configuration file that is used for building the
    installers. As the yaml file contains jinja templating,
    and because PyYAML cannot parse these (one cannot force
    string only-parsing!) we created a simple wrapper-script
    which can change key-value pairs in the yaml file.
    It is very basic, but we also do not need any advanced
    features.
    Currently it can:
    - add keys that are not existing
    - change key:value pairs
    - change list elements in key:list entries
    
    The current installers are using several environments
    allowing students to access course specific environments.
    This is to nudge students into *thinking* about environments.

---
+++ update_yaml.py
@@ -0,0 +1,136 @@
+#!/bin/env python3
+
+import sys
+from warnings import warn
+from io import StringIO, SEEK_END
+from pathlib import Path
+
+from yaml import safe_load, safe_dump
+
+def read_yaml(file: str | Path) -> StringIO:
+    """Parses and reads a yaml file with optional scheme versioning
+
+    It will return a stream of the content of the file.
+    Since the Miniforge3 `construct.yaml` file contains Jinja2 templates
+    we cannot parse it using PyYaml or other tools.
+
+    Therefore this reads it *as is*, then a subsequent key-edit change will
+    be done.
+    """
+    yaml = StringIO()
+    yaml.write(open(file, 'r').read())
+    yaml.seek(0)
+    return yaml
+
+
+def replace_key(stream: StringIO, key: str, value: str) -> StringIO:
+    """Replaces a key in the stream if found """
+    out = type(stream)()
+
+    key_s = f"{key}:"
+    found = False
+
+    for line in stream:
+        if line.startswith(key_s):
+            out.write(": ".join([key, value]) + "\n")
+            found = True
+        else:
+            out.write(line)
+
+    if not found:
+        raise Exception(f"Could not find {key=}")
+    out.seek(0)
+    return out
+
+
+def replace_list_element(stream: StringIO, key: str, element_start: str, value: str) -> StringIO:
+    out = type(stream)()
+
+    key_s = f"{key}:"
+    found = False
+
+    stream_iter = iter(stream)
+    while True:
+        try:
+            line = next(stream_iter)
+        except StopIteration:
+            break
+        out.write(line)
+        if line.startswith(key_s):
+            # Since we are in a list, we don't expect it
+            # to be the last, it's not elegant.
+            list_elem = next(stream_iter)
+            indent = "  " # default indentation for list specifications
+            while list_elem.strip().startswith(("- ", "# ")):
+                indent, element_value = list_elem.split("-", maxsplit=1)
+                if element_value.strip().startswith(element_start):
+                    found = True
+                    out.write("".join([indent, "- ", value, "\n"]))
+                else:
+                    out.write(list_elem)
+                list_elem = next(stream_iter)
+            if not found:
+                # add it
+                out.write("".join([indent, "- ", value, "\n"]))
+                warn(f"Adding element[{element_start}] to list {key}")
+                found = True
+
+            out.write(list_elem)
+
+    if not found:
+        raise Exception(f"Could not find {key=}")
+
+    out.seek(0)
+    return out
+
+
+def has_key(stream: StringIO, key:str) -> bool:
+    """Checks if a key exists in the yaml file """
+    key_s= f"{key}:"
+    found =False
+
+    for line in stream:
+        if line.startswith(key_s):
+            found = True
+            break
+
+    stream.seek(0)
+    return found
+
+
+# Do actual parsing and creation of the new constructor recipe.
+# This is hard-coded because it'll always be this one
+yaml_stream = read_yaml("miniforge/Miniforge3/construct.yaml")
+
+# Parse the yaml file with the data that is specific for DTU
+dtu_values = safe_load(open("dtu_constructor.yaml", 'r'))
+# Start by replacing the Python version
+yaml_stream = replace_list_element(yaml_stream, "specs", "python",
+                                   dtu_values.pop("dtu_python_version"))
+
+
+# Pop out the specification for the version
+# We know of these two we should replace
+for key in ("specs", "user_requested_specs"):
+    packages = dtu_values.pop(key)
+    for element in packages:
+        element_start, *_ = element.split(" ", maxsplit=1)
+        yaml_stream = replace_list_element(yaml_stream, key, element_start, element)
+
+
+# Check that there are no new keys!
+for key in dtu_values:
+    if has_key(yaml_stream, key):
+        raise NotImplementedError(f"Found the {key=}, but did not expect it!")
+
+# jump to the end
+yaml_stream.seek(0, SEEK_END)
+yaml_stream.write("\n")
+yaml_stream.write("# DTU specific settings\n")
+safe_dump(dtu_values, yaml_stream)
+
+if len(sys.argv) > 1:
+    out = sys.argv[1]
+else:
+    out = "constructor.yaml"
+open(out, 'w').write(yaml_stream.getvalue())
