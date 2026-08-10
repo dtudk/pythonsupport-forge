a06ecd28eebc4d0faaeb3164cb2a0c4b6453d186
Nick Papior <nickpapior@gmail.com>
2026-08-10 09:10:14 +0200

    trying another prefix

--- scripts/dtu_post_install.ps1
+++ scripts/dtu_post_install.ps1
@@ -4,19 +4,24 @@
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
+# DEBUG!
+$DEBUG_FILE = & Get-Location
+Get-ChildItem Env: | Out-File -FilePath $DEBUG_FILE\pis.debug
+Get-Variable | Out-File -FilePath $DEBUG_FILE\pis.debug -Append
+# DEBUG END!
+
 # Load conda's PowerShell hook from the newly installed prefix and
 # activate the base environment at that same prefix.
-& Join-Path "$Env:PREFIX" "\shell\condabin\conda-hook.ps1"
-conda activate "$Env:PREFIX"
+& Join-Path "$PREFIX" "\shell\condabin\conda-hook.ps1"
+conda activate "$PREFIX"
 
 # Initialize conda for all supported shells on this machine.
 conda init --all
 
 # Create PEP 668 EXTERNALLY-MANAGED marker to block pip/uv/poetry in base.
 # See PEP 668 for details.
-$Python_EXE = Join-Path "$Env:PREFIX" "\bin\python"
-$Python_STDLIB = & "$Python_EXE" -c "import sysconfig; print(sysconfig.get_path('stdlib',
-sysconfig.get_default_scheme()))"
+$Python_EXE = Join-Path "$PREFIX" "\bin\python"
+$Python_STDLIB = & "$Python_EXE" -c "import sysconfig; print(sysconfig.get_path('stdlib', sysconfig.get_default_scheme()))"
 $MarkerFile = Join-Path "$Python_STDLIB" "EXTERNALLY-MANAGED"
 
 $MarkerContent = @'
@@ -32,4 +37,4 @@ Error=This base environment is frozen and cannot be modified.
   https://pythonsupport.dtu.dk/learn-more/packages-and-environments/environments.html
 '@
 
-Set-Content -Path $MarkerFile -Value $MarkerContent -Encoding utf8
+Set-Content -Path $MarkerFile -Value "$MarkerContent" -Encoding utf8
